2fdcd9b4bee307f5beaf23bbc10119574299e61e
unseen <unseensa@gmail.com>
2014-10-27 18:42:57 +0200

    simple style

--- lesson-3/hw/Shooter.php
+++ lesson-3/hw/Shooter.php
@@ -8,26 +8,40 @@ class Shooter
 
     public function __construct()
     {
-        echo ' Enter weapon you want to shoot with!' . PHP_EOL . ' Available: Pistol, Machinegun, Bow, Rifle, Gun' . PHP_EOL;
+        echo PHP_EOL.' Enter weapon you want to shoot with!' . PHP_EOL . ' Available: Pistol, Machinegun, Bow, Rifle, Gun' . PHP_EOL;
         $this->weapon = trim(strtolower(str_replace(" ", "", fgets(STDIN))));
         echo ' Enter how much ammo you need!' . PHP_EOL . ' Example: 50, 100, 2000' . PHP_EOL;
-        $this->ammo = strtolower(str_replace(" ", "", fgets(STDIN)));
-        $this->shoot($this->weapon);
+        $this->ammo = trim(fgets(STDIN));
+        $this->validate($this->weapon, $this->ammo);
+        $this->shoot();
+        $this->reload($this->weapon, $this->ammo);
     }
 
-
-    /**
-     * @param $weapon
-     */
-    private function shoot($weapon)
+    private function validate($weapon, $ammo)
     {
+        $numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        $error = true;
         foreach ($this->weaponClip as $key => $value) {
             if ($key === $weapon) {
-                return $weapon;
+                $error = false;
             }
         }
-        echo ' Sorry, you are not skilled enough to use this weapon' . PHP_EOL;
-        $this->__construct();
+        if ($error) {
+            echo "\033[31m Sorry, you are not skilled enough to use this weapon\033[0m" . PHP_EOL;
+        }
+        for ($i = 0; $i < strlen($ammo); $i++) {
+            if (!in_array($ammo[$i], $numbers)) {
+                echo "\033[31m Ammo must be integer!\033[0m".PHP_EOL;
+                $error = true;
+            }
+        }
+        if ($error) {
+            $this->__construct();
+        }
+    }
+
+    private function shoot()
+    {
 
     }
 
--- lesson-3/hw/index.php
+++ lesson-3/hw/index.php
@@ -1,6 +1,6 @@
 <?php
 include_once('Shooter.php');
 
-echo PHP_EOL . ' Welcome to shooting simulator 1.0 :)' . PHP_EOL;
+echo PHP_EOL . "\033[32m Welcome to shooting simulator 1.0 :)\033[0m" . PHP_EOL;
 
 $shooter = new Shooter();
