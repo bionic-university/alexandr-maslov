c94bec8ad71af4aeadac7d1581ca7ebabda100b3
unseen <unseensa@gmail.com>
2014-10-27 17:51:53 +0200

    index file

--- lesson-3/hw/Shooter.php
+++ lesson-3/hw/Shooter.php
@@ -1,24 +1,38 @@
 <?php
+echo PHP_EOL . ' Welcome to shooting simulator 1.0 :)' . PHP_EOL;
 
 class Shooter
 {
-    private $weaponShootrate = ['pistol' => 13, 'machinegun' => 150, 'bow' => 10, 'rifle' => 20, 'gun' => 30];
+    private $weaponClip = ['pistol' => 13, 'machinegun' => 150, 'bow' => 10, 'rifle' => 20, 'gun' => 30];
+    private $weapon;
+    private $ammo;
 
     public function __construct()
     {
-        echo PHP_EOL.'Welcome to shooting simulator 1.0 :)'.PHP_EOL;
-        echo 'Enter weapon you want to shoot with!'.PHP_EOL.'Available: Pistol, Machinegun, Bow, Rifle, Gun'.PHP_EOL;
-        $weapon = strtolower(str_replace(" ", "", fgets(STDIN)));
-        echo 'Enter how much ammo you need!'.PHP_EOL.'Example: 50, 100, 2000'.PHP_EOL;
-        $ammo = strtolower(str_replace(" ", "", fgets(STDIN)));
+        echo ' Enter weapon you want to shoot with!' . PHP_EOL . ' Available: Pistol, Machinegun, Bow, Rifle, Gun' . PHP_EOL;
+        $this->weapon = trim(strtolower(str_replace(" ", "", fgets(STDIN))));
+        echo ' Enter how much ammo you need!' . PHP_EOL . ' Example: 50, 100, 2000' . PHP_EOL;
+        $this->ammo = strtolower(str_replace(" ", "", fgets(STDIN)));
+        $this->shoot($this->weapon);
     }
 
-    private function Shoot()
+
+    /**
+     * @param $weapon
+     */
+    private function shoot($weapon)
     {
+        foreach ($this->weaponClip as $key => $value) {
+            if ($key === $weapon) {
+                return $weapon;
+            }
+        }
+        echo ' Sorry, you are not skilled enough to use this weapon' . PHP_EOL;
+        $this->__construct();
 
     }
 
-    private function Reload()
+    private function reload($weapon, $ammo)
     {
 
     }
