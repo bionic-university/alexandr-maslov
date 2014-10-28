398020a211df0ff7a4c10b825e418ecc04417166
unseen <unseensa@gmail.com>
2014-10-28 14:53:35 +0200

    logic fixes

--- lesson-3/hw/Shooter.php
+++ lesson-3/hw/Shooter.php
@@ -13,28 +13,23 @@ class Shooter implements RetryInterface
         $this->weapon = trim(strtolower(str_replace(' ', '', fgets(STDIN))));
         echo ' Enter how much ammo you need!' . PHP_EOL . ' Example: 50, 100, 2000' . PHP_EOL;
         $this->ammo = trim(fgets(STDIN));
-        $this->validate($this->weapon, $this->ammo);
+        $this->validate();
         $this->shoot();
-        $this->reload($this->weapon, $this->ammo);
+        $this->reload();
     }
 
-
-    /**
-     * @param $weapon
-     * @param $ammo
-     */
-    private function validate($weapon, $ammo)
+    private function validate()
     {
         $error = true;
         foreach ($this->weaponClip as $key => $value) {
-            if ($key === $weapon) {
+            if ($key === $this->weapon) {
                 $error = false;
             }
         }
         if ($error) {
             echo "\033[31m Sorry, you are not skilled enough to use this weapon\033[0m" . PHP_EOL;
         }
-        if (!preg_match('/^[0-9]*$/', $ammo)) {
+        if (!preg_match('/^[0-9]*$/', $this->ammo)) {
             echo "\033[31m Ammo must be a natural number!\033[0m" . PHP_EOL;
             $error = true;
         }
@@ -54,26 +49,21 @@ class Shooter implements RetryInterface
 ..//___//' . PHP_EOL;
     }
 
-
-    /**
-     * @param $weapon
-     * @param $ammo
-     */
-    private function reload($weapon, $ammo)
+    private function reload()
     {
         $clipsUsed = 0;
         $ammoUnused = 0;
         foreach ($this->weaponClip as $key => $value) {
-            if (($key === $weapon) && ($ammo >= $value)) {
-                $clipsUsed = floor($ammo / $value);
-                $ammoUnused = $ammo % $value;
+            if (($key === $this->weapon) && ($this->ammo >= $value)) {
+                $clipsUsed = floor($this->ammo / $value);
+                $ammoUnused = $this->ammo % $value;
             }
-            if (($key === $weapon) && ($ammo < $value)) {
+            if (($key === $this->weapon) && ($this->ammo < $value)) {
                 $ammoUnused = 0;
                 $clipsUsed = 0;
             }
         }
-        echo "\033[32m Nice shooting! You have shot \033[0m" . ($ammo-$ammoUnused) . "\033[32m ammo\033[0m" . PHP_EOL;
+        echo "\033[32m Nice shooting! You have shot \033[0m" . ($this->ammo-$ammoUnused) . "\033[32m ammo\033[0m" . PHP_EOL;
         echo "\033[32m You have reloaded \033[0m" . $clipsUsed . "\033[32m times\033[0m" . PHP_EOL;
         echo "\033[32m You have \033[0m" . $ammoUnused . "\033[32m ammo left unused\033[0m" . PHP_EOL;
         echo ' Do you want to try again? [y/n]';
